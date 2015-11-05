2f85c7aa0c9cbc4daa3f399853d127206b9d4249
Jose Palomino Gallo <palomino@sukap0001.km.icrr.u-tokyo.ac.jp>
2015-11-05 19:51:33 +0900

    first commit

---
+++ scripts/mk_skdetsim.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+#f1 cardtemplate
+#f2 card for input
+#f3 c shell script
+
+import sys, os, random
+
+produname = "muons"
+nevents = 500
+nfiles = 20
+pmin = 230.
+pmax = 260.
+cardtemplate = "/home/santucci/fiTQun/skdetsim/sk4_odtune.card"
+skdetsimscript = "/home/santucci/fiTQun/skdetsim/skdetsim_high.sh" 
+scriptsdir = "/home/palomino/test/"
+outdir = "/disk/usr2/santucci"
+
+replacements = {'VECT-NEVT':'VECT-NEVT %s' % nevents, \
+        'VECT-MOM':'VECT-MOM %s %s' % (pmin,pmax) }
+linetemp = ""
+for file in range(0, nfiles):
+    os.system("mkdir -p %s/%s" %(outdir,file))
+    r1 = random.randint(100000000,1000000000)
+    r2 = random.randint(100000000,1000000000)
+    r3 = random.randint(100000000,1000000000)
+    replacements['VECT-RAND'] = 'VECT-RAND %s %s %s 0 0' % (r1,r2,r3)
+    f1 = open(cardtemplate, "r")
+    cardname = "%s/%s/%s%s.card" % (scriptsdir,file,produname,file) 
+    f2 = open(cardname, "w")
+    for line in f1:
+        linetemp = line
+        for src, replace in replacements.iteritems():
+            if line.split(" ")[0] == src:
+                linetemp = replace
+                break
+        f2.write( linetemp )
+    f3 = open("%s/%s/%s%s.csh" % (scriptsdir,file,produname,file), "w")
+    f3.write("#!/bin/tcsh\n")
+    f3.write("# \n# Batch mode using NQS \n#\n")
+    f3.write("# @\$-o %s/log/%s_%s.out \n" % (outdir,produname,file) )
+    f3.write("# @\$-e %s/log/%s_%s.err \n\n" % (outdir,produname,file) )
+    f3.write("source "+skdetsimscript+" "+cardname+" "+outdir+"/zbs/"+produname+"_%s.zbs" % file)
+
+    f2.close()
+    f1.close()
