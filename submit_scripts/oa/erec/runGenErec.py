f4b09ca7776ad3c9b7d49d0a73229a1835e98f65
palominogallo <jose.palominogallo@stonybrook.edu>
2016-03-22 03:20:21 +0900

    changing to python

---
+++ submit_scripts/oa/erec/runGenErec.py
@@ -0,0 +1,34 @@
+#! /usr/bin/python
+
+import sys, subprocess
+sys.path.append('/home/palomino/sk/submit_scripts/oa/include')
+import erec_settings
+
+if len(sys.argv)!= 6:
+	print('events, mode, potnu, potnub, syste')
+	exit()
+
+script, events, mode, potnu, potnub, syste = sys.argv
+
+ana, mainoutdir, codedir = erec_settings.BASIC()
+potneutrino, potantineutrino = erec_settings.POT(potnu, potnub)
+
+nevents = int(float(events) / 1000)
+subprocess.call(codedir+'/bin/GenerateXP' \
+		+' -f '   +mainoutdir+erec_settings.matrix \
+		+' -pe '  +mainoutdir+'/inputs/NuePDF/numode_nue_binning_J/' \
+		+' -pm '  +mainoutdir+'/inputs/NuMuPDF/numode_numu_binning_J.root' \
+		+' -pbe ' +mainoutdir+'/inputs/NuePDF/antinu_nue_binning_J/' \
+		+' -pbm ' +mainoutdir+'/inputs/NuMuPDF/antinu_numu_binning_J.root' \
+		+' -xe '  +mainoutdir+'/inputs/RFNue/nu_beam/' \
+		+' -xmu ' +mainoutdir+'/inputs/RFNuMu/nu_beam/' \
+		+' -xbe ' +mainoutdir+'/inputs/RFNue/nubar_beam/' \
+		+' -xbmu '+mainoutdir+'/inputs/RFNuMu/nubar_beam/' \
+		+' -tnb ' +potnub \
+		+' -tnu ' +potnu \
+		+' -n '   +events \
+		+' -m '   +mode \
+		+' -s '   +syste \
+		+' -sf '  +mainoutdir+'/inputs/SplineFile/weightsv8_t2ksk.14a.neut5.3.2.13a_tuned_v1r0.250ka.fine.numubar_x_numubar_numuselec.root' \
+		+' -o '   +mainoutdir+'/'+ana+'/generate/gen_n'+str(nevents)+'k_m'+mode+'_nu'+potneutrino+'_nubar'+potantineutrino+'_s'+syste+'.root'
+		, shell=True)
---
+++ submit_scripts/oa/erec/runTempErec.py
@@ -0,0 +1,32 @@
+#! /usr/bin/python
+
+import sys, subprocess
+sys.path.append('/home/palomino/sk/submit_scripts/oa/include')
+import erec_settings
+
+if len(sys.argv)!= 4:
+	print('events,  potnu, potnub')
+	exit()
+
+script, events, potnu, potnub = sys.argv
+ana, mainoutdir, codedir = erec_settings.BASIC()
+potneutrino, potantineutrino = erec_settings.POT(potnu, potnub)
+
+nevents = int(float(events) / 1000)
+subprocess.call(codedir+'/bin/MakeTemplate' \
+		+' -f '   +mainoutdir+erec_settings.matrix \
+		+' -pe '  +mainoutdir+'/inputs/NuePDF/numode_nue_binning_J/' \
+		+' -pm '  +mainoutdir+'/inputs/NuMuPDF/numode_numu_binning_J.root' \
+		+' -pbe ' +mainoutdir+'/inputs/NuePDF/antinu_nue_binning_J/' \
+		+' -pbm ' +mainoutdir+'/inputs/NuMuPDF/antinu_numu_binning_J.root' \
+		+' -xe '  +mainoutdir+'/inputs/RFNue/nu_beam/' \
+		+' -xmu ' +mainoutdir+'/inputs/RFNuMu/nu_beam/' \
+		+' -xbe ' +mainoutdir+'/inputs/RFNue/nubar_beam/' \
+		+' -xbmu '+mainoutdir+'/inputs/RFNuMu/nubar_beam/' \
+		+' -tnb ' +potnub \
+		+' -tnu ' +potnu \
+		+' -n '   +events \
+		+' -sf '  +mainoutdir+'/inputs/SplineFile/weightsv8_t2ksk.14a.neut5.3.2.13a_tuned_v1r0.250ka.fine.numubar_x_numubar_numuselec.root' \
+		+' -ft '  +mainoutdir+erec_settings.systhrows \
+		+' -o '   +mainoutdir+'/'+ana+'/template/tpl_t'+str(nevents)+'k_nu'+potneutrino+'_nubar'+potantineutrino+'.root' \
+		, shell=True)
