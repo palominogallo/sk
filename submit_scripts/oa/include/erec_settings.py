d7dce8d6ed3ad87999be2f3fc7a41174afcd2eed
palominogallo <jose.palominogallo@stonybrook.edu>
2016-04-21 00:04:26 +0900

    adding settgins python

---
+++ submit_scripts/oa/include/erec_settings.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+def BASIC():
+	agroup = 'erec'
+	outdir = '/disk/usr2/palomino/NuMu16/'
+	exedir = '/home/palomino/ptheta/P-theta/Minimal/'
+	return agroup, outdir, exedir
+
+matrix    = '/inputs/Matrix/FullErecMatrix_postfit_xsec2015c_1p1h.root'
+systhrows = '/inputs/Throws/sys_throws_erec_xsec2015c_1p1h.root'
---
+++ submit_scripts/oa/include/ptheta_settings.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+def BASIC():
+	agroup = 'ptheta'
+	outdir = '/disk/usr2/palomino/NuMu16/'
+	exedir = '/home/palomino/ptheta/P-theta/Minimal/'
+	return agroup, outdir, exedir
+
+matrix    = '/inputs/Matrix/FullPthetaMatrix_postfit_xsec2015c_1p1h.root'
+systhrows = '/inputs/Throws/sys_throws_ptheta_xsec2015c_1p1h.root'
